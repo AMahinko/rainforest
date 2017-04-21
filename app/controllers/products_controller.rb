8c486e440fb2ed949a31c85cd8f7bca159cc1efe
Alex <alex.mahinko@gmail.com>
2017-04-21 12:12:49 -0400

    products controller set up, pending testing.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,49 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products =  Products.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+
+    @picture = Product.new
+
+  end
+
+  def edit
+
+    @picture = Product.find(params[:id])
+
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if Product.save
+      redirect_to_products_url
+      return
+    else
+      render :new
+      return
+    end
+  end
+
+  def update
+    @product =  Product.find(product_params)
+    if @product.update_attributes(product_params)
+      redirect_to_products_url
+      return
+    else
+      render :edit
+      return
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  # get '/products' 'products#index'
+  # get '/products' 'products#create'
+  # get '/products/new' 'products#new'
+  # get '/products/:id/edit' 'products#edit'
+  # get '/products/:id' 'products#show'
+  # patch '/products/:id' 'products#update'
+  # put 'products/:id' 'products#update'
+  # delete 'products/:id' 'products#delete'
+
+
+  resources :products
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
